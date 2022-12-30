f6bb5f5239266cce3f41c33c7c15e4abd760399c
Betul Alptekin <f16btl@gmail.com>
2022-12-30 17:35:10 +0300

    v2

---
+++ src/test/day07/Top.java
@@ -0,0 +1,36 @@
+package test.day07;
+
+public class Top {
+
+    public static void main(String[] args) {
+
+        // Bir top belirli yükseklikten atılmaktadır.
+        // Atıldıktan sonra, atıldığı yüksekliğin 3/4 u kadar yerden yukarı doğru zıplamaktadır
+        // Top zıplama  yüksekliği 1 metrenin altına indiğinde durmaktadır.
+        // Bu ana kadar aldigi toplam yolu ve yere vurma sayısını bulan
+        // do while code blogu create ediniz.
+
+        double yukseklik = 2.00;
+        int yereVurmaSayisi = 0;
+        double topunToplamYolu = 0;
+
+        do {
+            topunToplamYolu+=yukseklik;
+            yereVurmaSayisi++;
+            yukseklik*=0.75;
+            topunToplamYolu+=yukseklik;
+
+        }while(yukseklik>=1);
+
+        System.out.println("Topun aldigi toplam yol = "+ topunToplamYolu+
+                "\nTopun yere vurma sayisi = "+yereVurmaSayisi);
+
+
+
+
+
+    }
+
+    }
+
+
